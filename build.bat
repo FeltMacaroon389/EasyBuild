949add7d17c0281b91a6644f4f6c131fee8231fc
FeltMacaroon389 <snorre@svedman.no>
2024-02-06 13:20:14 +0100

    Added Comments, and icon support.

--- build.bat
+++ build.bat
@@ -1,15 +1,36 @@
 @echo off
-
 setlocal enabledelayedexpansion
 
+REM Define variables
+set "icoFile="
+set "filename="
+
+REM Identify ICO file in the directory
+for %%i in (*.ico) do (
+    if not defined icoFile (
+        set "icoFile=%%i"
+    ) else (
+        echo Error: Multiple ICO files found in the directory. Script might not work as expected.
+    )
+)
+
+if not defined icoFile (
+    echo.
+    echo Error: No ICO file found in the directory. EXE will have default icon.
+)
+
+REM Process Python files
 for %%f in (*.py) do (
     set "filename=%%~nf"
-    pyinstaller --onefile %%f
+    
+    REM Use ICO file with pyinstaller
+    pyinstaller --onefile --icon=!icoFile! %%f
+    
     rd /s /q build
     del %%~nf.spec
-    ren dist Built_EXE
-    cd Built_EXE
-    cd ..
+    
+    REM Move the output directory to a new name
+    ren dist Built_EXE_!filename!
 )
 
 endlocal
